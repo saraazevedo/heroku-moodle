a7ee762d9bf318686d188eb7924786c735d74b1d
rafaellucio <rafaelantoniolucio@gmail.com>
2014-02-03 00:05:26 -0200

    create structure connect db

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -8,6 +7,31 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'moodle_project'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM users', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows);
+});
+
+connection.end();
+
+// var connection = mysql.createConnection({
+// 	host: 'us-cdbr-east-05.cleardb.net',
+// 	user: 'b7d0fa1174c963',
+// 	password: 'b73df636',
+// 	database: 'heroku_3ab538e8ba3566a'
+// });
 
 var app = express();
 
@@ -24,12 +48,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
+
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
 });
